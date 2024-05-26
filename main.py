7057758fab7453da51fce84466a7f050b326b876
shalinipal <palshalini23@gmail.com>
2024-05-26 22:41:49 +0200

    structuring the process flow
    created the main file and model.py file

---
+++ main.py
@@ -0,0 +1,18 @@
+#1. import the libraries
+
+
+#2. importing the constants
+
+
+#3. data loading
+
+
+#4. preprocessing of data
+
+
+#5. fitting the data into model
+
+
+#6. evaluation of the model performance
+
+
